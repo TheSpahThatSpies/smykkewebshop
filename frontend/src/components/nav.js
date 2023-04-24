20263e2922d0b4281d85b1f43d86d62ae0a84057
jacobgervin <jacobgervin@gmail.com>
2023-04-24 21:28:03 +0200

    Changes

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -11,6 +11,7 @@ function Nav() {
       <span class="self-center text-2xl font-semibold whitespace-nowrap text-slate-950">JOC</span>
   </a>
   <div class="flex md:order-2">
+
     <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
       <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
       <span class="sr-only">Search</span>
@@ -22,6 +23,11 @@ function Nav() {
       </div>
       <input type="text" id="search-navbar" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 bg-slate-100 focus:ring-slate-500 focus:border-slate-500" placeholder="Search..." />
     </div>
+    <button className='ml-2 mr-2'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+        </svg>
+    </button>
     <button data-collapse-toggle="navbar-search" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
       <span class="sr-only">Open menu</span>
       <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -34,15 +40,18 @@ function Nav() {
         </div>
         <input type="text" id="search-navbar" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..."></input>
       </div>
-      <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg  md:flex-row md:space-x-8 md:mt-0">
+      <ul class="flex flex-col p-4 md:p-0 mt-4 font-light rounded-lg  md:flex-row md:space-x-8 md:mt-0">
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-slate-950 rounded md:p-0" aria-current="page">Home</a>
+          <a href="#" class="block py-2 pl-3 pr-4 text-slate-950 rounded md:p-0" aria-current="page">Rings</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-slate-950 rounded md:p-0 ">About</a>
+          <a href="#" class="block py-2 pl-3 pr-4 text-slate-950 rounded md:p-0 ">Bracelets</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-slate-950 rounded md:p-0">Services</a>
+          <a href="#" class="block py-2 pl-3 pr-4 text-slate-950 rounded md:p-0">Earrings</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-slate-950 rounded md:p-0">Necklaces</a>
         </li>
       </ul>
     </div>
