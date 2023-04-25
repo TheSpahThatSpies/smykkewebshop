b97f4954fd3ad679ef2c1fa18d1a8442f49a5eea
outman1998 <outmanmounir98@gmail.com>
2023-04-25 12:44:50 +0200

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Hero from './components/indexpage/Hero';
 import MostPopular from './components/indexpage/MostPopular';
 import Section from './components/indexpage/Section';
+import Footer from './components/indexpage/Footer';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <Hero></Hero>
     <MostPopular></MostPopular>
     <Section></Section>
+    <Footer></Footer>
   </>
 
   
---
+++ frontend/src/components/indexpage/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styling from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <>
+    <div className={styling.containeren}>
+        
+        <div className={styling.first}>
+            <h1>More About Company</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+
+        <div className={styling.second}>
+            <h1>Keep Connected</h1>
+            <p>Like us on Facebook</p>
+            <p>Follow us on Instagram</p>
+            <p>Follow us on Pinterest</p>
+        </div>
+
+        <div className={styling.tredje}>
+            <h1>Contact Information</h1>
+            <p>joc@jewelry.com</p>
+            <p>+45 25 11 22 05</p>
+        </div>
+
+    </div>
+    </>
+  )
+}
