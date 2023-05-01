1f26328eda1edb30217fe973ed632c3868f444e5
outman1998 <outmanmounir98@gmail.com>
2023-05-01 22:23:57 +0200

    last functionality for the basket

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -11,29 +11,29 @@ export default function Card() {
 
     const totalAmount = getTotalCartAmount();
 
+    const UI = totalAmount > 0 ? <div className={styling.container}>
+    <h1 className={styling.overskrift}>your card items</h1>
+    <ul class=" divide-y divide-gray-200 dark:divide-gray-700">
+      <li className='listtt'>
+        {PRODUCTS.map((product) => {
+          if(cartItems[product.id] !== 0) {
+          return <CardItem data={product}/>
+          }
+        })}
+      </li>
+    </ul>
+    <div className={styling.center}>
+      <p>total: <span>{totalAmount} DKK</span></p>
+      <Link to={"/"}>
+        <button className={styling.continue}>Continue shopping</button>
+      </Link>
+      <button className={styling.checkOut}>Check out</button>
+    </div>
+  </div> : <p className={styling.emptyBasket}>Your shopping basket is empty</p>
+
   return (
     <>
-
-        <div className={styling.container}>
-          <h1 className={styling.overskrift}>your card items</h1>
-          <ul class=" divide-y divide-gray-200 dark:divide-gray-700">
-            <li className='listtt'>
-              {PRODUCTS.map((product) => {
-                if(cartItems[product.id] !== 0) {
-                return <CardItem data={product}/>
-                }
-              })}
-            </li>
-          </ul>
-          <div className={styling.center}>
-            <p>total: <span>{totalAmount} DKK</span></p>
-            <Link to={"/"}>
-              <button className={styling.continue}>Continue shopping</button>
-            </Link>
-            <button className={styling.checkOut}>Check out</button>
-          </div>
-        </div>
-
+    {UI}
     </>
   )
 }
