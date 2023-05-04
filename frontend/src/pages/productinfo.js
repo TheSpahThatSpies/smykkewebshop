09672ef4245910215838ae3677575c5f151eceea
jacobgervin <jacobgervin@gmail.com>
2023-05-04 12:08:23 +0200

    Stock information + dkk price

--- frontend/src/pages/productinfo.js
+++ frontend/src/pages/productinfo.js
@@ -10,7 +10,7 @@ function Productinfo() {
   const { products } = useContext(ShopContext);
   const product = products.find((product) => product.id === parseInt(id));
 
-  const { name, category, price, description, image } = product.acf;
+  const { name, category, price, description, image, stock } = product.acf;
 
   const [added, setAdded] = useState(false);
 
@@ -41,13 +41,20 @@ function Productinfo() {
             <p className='text-md'>
            {description}
             </p>
-            <p className='text-md'>${price}</p>
+            <p className='text-md'>{price} DKK</p>
             <button class="border-2 border-slate-800 w-full md:w-64  py-2 text-slate-800 cursor-pointer hover:bg-gray-800 hover:text-gray-200 flex justify-center" onClick={handleAddToCart} >
           {added ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0" stroke="currentColor" class="w-6 h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
     </svg>  : 'Add to cart'}
         </button>
-            <p className='text-md'>Stock information</p>
+        {stock > 0 ? (
+        <div className="flex items-center mt-3">
+            <div className="w-3 h-3 rounded-full bg-green-500 mr-2"></div>
+            <p className='text-md'>In Stock</p>
+        </div>
+    ) : (
+        <p className='text-md text-red-500'>Out of Stock</p>
+    )}
         </div>
     </div>
     <Footer></Footer>
