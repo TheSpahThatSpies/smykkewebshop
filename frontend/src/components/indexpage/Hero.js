6bb27a9e2f3859ee124b1a5314edbe1e7a0322b5
jacobgervin <jacobgervin@gmail.com>
2023-05-04 02:20:45 +0200

    hero link

--- frontend/src/components/indexpage/Hero.js
+++ frontend/src/components/indexpage/Hero.js
@@ -15,9 +15,21 @@ export default function hero() {
             <div className={styling.herocontent}>
             <h1>Jewelry by JOC</h1>
             <p>Handcrafted by professionals</p>
-            <Link to={"/products"}>
-              <button>Products</button>
-            </Link>
+            <ul class="flex p-4 md:p-0 md:mt-6 font-thin rounded-lg justify-evenly md:justify-normal  flex-row md:space-x-8 ">
+      
+              <li>
+                <Link to={"/products/rings"} className="block py-2 pl-3 pr-4 text-white rounded md:p-0 navlink">Rings</Link>
+              </li>
+              <li>
+                <Link to={"/products/necklaces"} className="block py-2 pl-3 pr-4 text-white rounded md:p-0 navlink">Necklaces</Link>
+              </li>
+              <li>
+                <Link to={"/products/bracelets"} className="block py-2 pl-3 pr-4 text-white rounded md:p-0 navlink">Bracelets</Link>
+              </li>
+              <li>
+                <Link to={"/products/earrings"} className="block py-2 pl-3 pr-4 text-white rounded md:p-0 navlink">Earrings</Link>
+              </li>
+            </ul>
         </div>
 
         </div>
--- frontend/src/components/indexpage/Section.js
+++ frontend/src/components/indexpage/Section.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styling from './Section.module.css';
-import { Link } from 'react-router-dom';
 
 
 export default function Section() {
@@ -8,25 +7,14 @@ export default function Section() {
     <>
 
     <div className={styling.container}>
+      
 
         <div className={styling.first}>
             <h1 className={styling.showlove}>Show your love</h1>
             <p className={styling.txt}>Get the perfect gift for your girlfriend, mom or wife</p>
             <p className={styling.txtSmall}>Celebrate the most important women in your life this Mother's Day with our exquisite jewelry collection. Show your love with a gift that is as unique and special as she is. Our selection of necklaces, bracelets, earrings, and rings offers a wide range of styles and designs to choose from, ensuring you find the perfect gift for your mom, girlfriend, or wife. Each piece is crafted with care and attention to detail, using only the finest materials, to make sure your gift will be treasured for years to come. Don't miss the chance to express your love and appreciation for the amazing women in your life with our beautiful Mother's Day collection.</p>
-            <Link to={"/products"}>
-              <button>Products</button>
-            </Link>
         </div>
 
-        <div className={styling.second}>
-            <h1>Check out our</h1>
-            <div className={styling.flex}>
-                <p>Rings</p>
-                <p>Bracelets</p>
-                <p>Earrings</p>
-                <p>Necklace</p>
-            </div>
-        </div>
 
     </div>
 
